c8a7f28f0bcd135988bc58096b74e7b7deb920f6
Rrahullkumar <rahulkr811844@gmail.com>
2025-02-15 17:43:39 +0530

    updated config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+const API_BASE_URL = "https://student-portal-backend-1qe5dz78q-rahul-kumars-projects-1a474def.vercel.app" || "http://localhost:5000";
 export default API_BASE_URL;
